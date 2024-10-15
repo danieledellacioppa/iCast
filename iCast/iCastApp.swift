8dc5e2666af69539f5cb1aef799c7d02048f8822
Daniele Della Cioppa <daniele.dellacioppa@gmail.com>
2024-10-15 17:40:18 +0200

    first commit

---
+++ iCast/iCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCastApp.swift
+//  iCast
+//
+//  Created by Daniele Della Cioppa on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct iCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iCastTests/iCastTests.swift
@@ -0,0 +1,17 @@
+//
+//  iCastTests.swift
+//  iCastTests
+//
+//  Created by Daniele Della Cioppa on 15/10/2024.
+//
+
+import Testing
+@testable import iCast
+
+struct iCastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
